6a7c8b715b87e491dddab090fd6d70d521ded153
Ivan <ivanmr932@gmail.com>
2021-08-02 16:31:15 -0400

    set production port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const http = require('http')
 const server = http.createServer(app)
